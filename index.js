78651d5435816985db743d58124b74ec743db295
Roman_Pylypyak <roman.lviv@yahoo.com>
2018-11-23 10:31:35 +0000

    Initial

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
    
-   const a = parseInt(prompt("Enter parameter a"));
-    const b = parseInt(prompt("Enter parameter b"));
-    const c = parseInt(prompt("Enter parameter c"));
+    const a = parseFloat(prompt("Enter parameter a"));
+    const b = parseFloat(prompt("Enter parameter b"));
+    const c = parseFloat(prompt("Enter parameter c"));
+    if (isNaN(a && b && c)){
+	alert('Enter Numbers')
+    }
+    else {alert (quad(a,b,c));
+    }
+function quad(a,b,c){
     let d=b*b-4*a*c;
     if (d<0){
-        alert('error')
+        alert('D<0. No answer')
     }
     let x1 = (-b+Math.sqrt(d))/(2*a);
     let x2 = (-b-Math.sqrt(d))/(2*a);
-    alert('x1= ' + x1 + ', ' + 'x2= '+ x2);
+    return('x1= ' + x1 + ', ' + 'x2= '+ x2);
+}
